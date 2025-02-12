cfb2577171285dc4d0d998095845d33741970917
Vinicius <vinyvick3@gmail.com>
2025-02-12 15:13:18 -0300

    Repositories Created

---
+++ src/main/java/com/barbearia/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.barbearia.repository;
+
+import com.barbearia.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
